68456de5f696c274541a1708c27ea87f6cd8f105
Recio Martínez-Charco <anrh@gft.com>
2023-04-20 13:13:02 +0200

    new junit5 test

--- src/test/java/microservicesgft/teammicroservicesgft/services/CatalogServiceTest.java
+++ src/test/java/microservicesgft/teammicroservicesgft/services/CatalogServiceTest.java
@@ -1,34 +1,33 @@
-package microservicesgft.teammicroservicesgft;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+package microservicesgft.teammicroservicesgft.services;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import microservicesgft.teammicroserviciesgft.models.Product;
 import microservicesgft.teammicroserviciesgft.repositories.ProductRepository;
 import microservicesgft.teammicroserviciesgft.services.CatalogService;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class CatalogServiceTest {
+
     @InjectMocks
     private CatalogService catalogService;
+   
     @Mock
     private ProductRepository productRepository;
-
-    
-
-    List<Product>catalog =new ArrayList<>();
-    
-
+   
+    private List<Product> catalog = new ArrayList<>();
+   
     @BeforeEach
     void setUp(){
         catalog.add(new Product(1, "maceta","50L", 10.00, 3));
@@ -41,9 +40,6 @@ public class CatalogServiceTest {
     List<Product>result=catalogService.findAllProducts();
     Object[]catalogArr=catalog.toArray();
     Object[] resultArr=result.toArray();
-        assertArrayEquals(catalogArr,resultArr);
-
+    assertArrayEquals(catalogArr,resultArr);
   }
-
-
 }
